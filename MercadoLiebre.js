68a995f4cd421e03ee37ced39f5f148f58e49e1a
unknown <rarbtk10@gmail.com>
2020-09-16 00:42:52 -0300

    Commit de MercadoLiebre

---
+++ MercadoLiebre.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+let path = require('path');
+
+app.get('/', function(req, res) {
+    let file = path.resolve("MercadoLiebre.html");
+  
+    res.sendFile(file);
+});
+
+
+
+app.get("*", function(req,res){
+    if(req.url.includes(".")){
+        
+        let file = path.resolve("./"+ req.url)
+        return res.sendFile(file)
+    }
+    
+})
+
+
+
+app.listen(3000, function() {
+  console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
